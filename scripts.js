697e2d27c0b144916488c47e6db9f7b9e0a52694
Ella R <61467087+EleonoraR@users.noreply.github.com>
2023-05-05 18:28:03 -0400

    Quotes Files are uploaded
    html, css, js

---
+++ scripts.js
@@ -0,0 +1,37 @@
+const btn = document.querySelector('.btn-motivation');
+
+const motivation = function () {
+  alert('works');
+};
+btn.addEventListener('click', logJSONData);
+
+async function logJSONData() {
+  let randomColor = Math.floor(Math.random() * 16777210).toString(16);
+  document.querySelector(
+    '.motivation-txt'
+  ).style.backgroundColor = `#${randomColor}`;
+  document.querySelector(
+    '.motivation-author'
+  ).style.backgroundColor = `#${randomColor}`;
+  document.querySelector('.motivation-txt').style.display = 'block';
+  document.querySelector('.motivation-author').style.display = 'block';
+
+  const response = await fetch('https://type.fit/api/quotes');
+  const jsonData = await response.json();
+
+  let randomQuote = Math.floor(Math.random() * 1643);
+  if (randomQuote <= 1643) {
+    document.querySelector(
+      '.motivation-txt'
+    ).textContent = `${jsonData[randomQuote].text}`;
+    if (jsonData[randomQuote].author === null) {
+      document.querySelector(
+        '.motivation-author'
+      ).textContent = `Unknown Author`;
+    } else {
+      document.querySelector(
+        '.motivation-author'
+      ).textContent = `${jsonData[randomQuote].author}`;
+    }
+  }
+}
